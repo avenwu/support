b5c7ab5db0f73e0084d9d44287459971336e0a2a
Chaobin Wu <iamavenwu@gmail.com>
2015-02-03 11:33:50 +0800

    demo of poterduff preview

---
+++ sample/src/main/java/com/avenwu/deepinandroid/ColorFilterDemo.java
@@ -0,0 +1,51 @@
+package com.avenwu.deepinandroid;
+
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+/**
+ * Created by chaobin on 2/3/15.
+ */
+
+public class ColorFilterDemo extends ActionBarActivity implements AdapterView.OnItemSelectedListener {
+    @InjectView(R.id.spinner)
+    Spinner mPorterDuffSpinner;
+    @InjectView(R.id.iv_preview)
+    ImageView mPreviewView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.filter_layout);
+        ButterKnife.inject(this);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.porter_duff_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        mPorterDuffSpinner.setAdapter(adapter);
+        mPorterDuffSpinner.setOnItemSelectedListener(this);
+    }
+
+    @SuppressWarnings("Unchecked")
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        PorterDuff.Mode mode = PorterDuff.Mode.class.getEnumConstants()[position];
+        mPreviewView.setColorFilter(Color.GREEN, mode);
+        Toast.makeText(this, "Select " + mode, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
