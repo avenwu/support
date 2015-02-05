bbc9b434ffa02a3010d9e7022d5c9661fb00b739
Chaobin Wu <iamavenwu@gmail.com>
2015-02-05 19:09:22 +0800

    add tab listener

--- support/src/main/java/net/avenwu/support/widget/FlatTabGroup.java
+++ support/src/main/java/net/avenwu/support/widget/FlatTabGroup.java
@@ -17,10 +17,12 @@ import android.widget.RadioGroup;
 
 import net.avenwu.support.R;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Created by chaobin on 2/4/15.
  */
-public class FlatTabGroup extends RadioGroup {
+public class FlatTabGroup extends RadioGroup implements RadioGroup.OnCheckedChangeListener {
     public FlatTabGroup(Context context) {
         this(context, null);
     }
@@ -31,6 +33,9 @@ public class FlatTabGroup extends RadioGroup {
     private String[] mItemString;
     private float mTextSize;
     private ColorStateList mTextColor;
+    private int[] mTabViewIds;
+    private OnTabCheckedListener mTabCheckedListener;
+    private OnCheckedChangeListener mListener;
 
     public FlatTabGroup(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -47,12 +52,15 @@ public class FlatTabGroup extends RadioGroup {
         mItemString = isInEditMode() ? new String[]{"TAB A", "TAB B", "TAB C"} : context.getResources().getStringArray(id);
         generateTabView(context, attrs);
         updateChildBackground();
+        super.setOnCheckedChangeListener(this);
     }
 
     private void generateTabView(Context context, AttributeSet attrs) {
         if (mItemString == null) {
             return;
         }
+        mTabViewIds = new int[mItemString.length];
+        int i = 0;
         for (String text : mItemString) {
             RadioButton button = new RadioButton(context, attrs);
             button.setGravity(Gravity.CENTER);
@@ -60,8 +68,36 @@ public class FlatTabGroup extends RadioGroup {
             button.setText(text);
             button.setTextColor(mTextColor);
             button.setTextSize(TypedValue.COMPLEX_UNIT_PX, mTextSize);
+            int id = generateViewId();
+            button.setId(mTabViewIds[i++] = id);
             addView(button, new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, 1));
         }
+    }
+
+    public void setOnTabCheckedListener(OnTabCheckedListener listener) {
+        mTabCheckedListener = listener;
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        if (mListener != null) {
+            mListener.onCheckedChanged(group, checkedId);
+        }
+        if (mTabCheckedListener != null) {
+            int checkedPosition = -1;
+            for (int i = 0; i < mTabViewIds.length; i++) {
+                if (mTabViewIds[i] == checkedId) {
+                    checkedPosition = i;
+                    break;
+                }
+            }
+            mTabCheckedListener.onChecked(this, checkedPosition);
+        }
+    }
+
+    @Override
+    public void setOnCheckedChangeListener(OnCheckedChangeListener listener) {
+        mListener = listener;
     }
 
     @Override
@@ -117,4 +153,27 @@ public class FlatTabGroup extends RadioGroup {
         return shape;
     }
 
+    private static final AtomicInteger sNextGeneratedId = new AtomicInteger(1);
+
+    /**
+     * Generate a value suitable for use in {@link #setId(int)}.
+     * This value will not collide with ID values generated at build time by aapt for R.id.
+     *
+     * @return a generated ID value
+     */
+    public static int generateViewId() {
+        for (; ; ) {
+            final int result = sNextGeneratedId.get();
+            // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
+            int newValue = result + 1;
+            if (newValue > 0x00FFFFFF) newValue = 1; // Roll over to 1, not 0.
+            if (sNextGeneratedId.compareAndSet(result, newValue)) {
+                return result;
+            }
+        }
+    }
+
+    public static interface OnTabCheckedListener {
+        public void onChecked(FlatTabGroup group, int position);
+    }
 }
